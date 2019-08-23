9a5554e6ccee91e33d25ee6f64c4f5c89d7c81b2
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2019-08-23 04:09:47 -0300

    adding more animations

--- Scripts/Animation.gd
+++ Scripts/Animation.gd
@@ -9,14 +9,21 @@ func _process(delta):
 		get_parent().get_node("Shooting").play("shoot down")
 	else:
 		get_parent().get_node("Shooting").play("hide")
+	if Input.is_action_pressed("ui_accept"):
+		get_parent().get_node("Squash Jump").play("Squash")
+		get_parent().get_node("AnimationPlayer").set_current_animation("Jump")
+	elif int(get_parent().get_parent().motion.x) > 100 and get_parent().get_parent().ground_detect():
+		get_parent().get_node("AnimationPlayer").set_speed_scale(get_parent().get_parent().motion.x/500) 
+		get_parent().get_node("AnimationPlayer").set_current_animation("Walk")
+	elif int(get_parent().get_parent().motion.x) < -100 and get_parent().get_parent().ground_detect():
+		get_parent().get_node("AnimationPlayer").set_speed_scale(get_parent().get_parent().motion.x/500) 
+		get_parent().get_node("AnimationPlayer").set_current_animation("Walk")
+	elif int(get_parent().get_parent().motion.x) == 0:
+		get_parent().get_node("AnimationPlayer").set_current_animation("Idle")
+	if get_parent().get_parent().ledge_detect() and not get_parent().get_parent().ground_detect():
+		get_parent().get_node("AnimationPlayer").set_current_animation("ledge grab")
 	if int(get_parent().get_parent().motion.x) > 100:
 		get_parent().set_scale(Vector2(-1,1))
-		get_parent().get_node("AnimationPlayer").set_speed_scale(get_parent().get_parent().motion.x/500) 
-		get_parent().get_node("AnimationPlayer").set_current_animation("Walk")
 	if int(get_parent().get_parent().motion.x) < -100:
 		get_parent().set_scale(Vector2(1,1))
-		get_parent().get_node("AnimationPlayer").set_speed_scale(get_parent().get_parent().motion.x/500) 
-		get_parent().get_node("AnimationPlayer").set_current_animation("Walk")
-	if int(get_parent().get_parent().motion.x) == 0:
-		get_parent().get_node("AnimationPlayer").set_current_animation("Idle")
 #	pass
