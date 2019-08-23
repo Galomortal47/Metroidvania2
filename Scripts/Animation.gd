4ac6fbed199586ac8f231196916f001307feb96e
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2019-08-23 03:09:06 -0300

    animation fixes

--- Scripts/Animation.gd
+++ Scripts/Animation.gd
@@ -4,9 +4,11 @@ func _process(delta):
 	print(int(get_parent().get_parent().motion.x))
 	if int(get_parent().get_parent().motion.x) > 100:
 		get_parent().set_scale(Vector2(-1,1))
+		get_parent().get_node("AnimationPlayer").set_speed_scale(get_parent().get_parent().motion.x/500) 
 		get_parent().get_node("AnimationPlayer").set_current_animation("Walk")
 	if int(get_parent().get_parent().motion.x) < -100:
 		get_parent().set_scale(Vector2(1,1))
+		get_parent().get_node("AnimationPlayer").set_speed_scale(get_parent().get_parent().motion.x/500) 
 		get_parent().get_node("AnimationPlayer").set_current_animation("Walk")
 	if int(get_parent().get_parent().motion.x) == 0:
 		get_parent().get_node("AnimationPlayer").set_current_animation("Idle")
