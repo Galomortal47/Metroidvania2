47f69df5f35532df1b47e68dcdcecba024bdf5f9
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2019-08-23 03:27:04 -0300

    animations

--- Scripts/Animation.gd
+++ Scripts/Animation.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
 func _process(delta):
-	print(int(get_parent().get_parent().motion.x))
+	if Input.is_action_pressed("ui_cancel"):
+		get_parent().get_node("Shooting").play("shoot")
+	elif Input.is_action_pressed("ui_up"):
+		get_parent().get_node("Shooting").play("shoot up")
+	elif Input.is_action_pressed("ui_down"):
+		get_parent().get_node("Shooting").play("shoot down")
 	if int(get_parent().get_parent().motion.x) > 100:
 		get_parent().set_scale(Vector2(-1,1))
 		get_parent().get_node("AnimationPlayer").set_speed_scale(get_parent().get_parent().motion.x/500) 
