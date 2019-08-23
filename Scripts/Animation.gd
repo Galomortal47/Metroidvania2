7f6ae126f730b6a991550c83fb7992e4014cdf93
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2019-08-23 02:58:46 -0300

    modify anim

--- Scripts/Animation.gd
+++ Scripts/Animation.gd
@@ -1,8 +1,13 @@
 extends Node2D
 
 func _process(delta):
-	if get_parent().get_parent().motion.x > 0:
+	print(int(get_parent().get_parent().motion.x))
+	if int(get_parent().get_parent().motion.x) > 100:
 		get_parent().set_scale(Vector2(-1,1))
-	else:
+		get_parent().get_node("AnimationPlayer").set_current_animation("Walk")
+	if int(get_parent().get_parent().motion.x) < -100:
 		get_parent().set_scale(Vector2(1,1))
+		get_parent().get_node("AnimationPlayer").set_current_animation("Walk")
+	if int(get_parent().get_parent().motion.x) == 0:
+		get_parent().get_node("AnimationPlayer").set_current_animation("Idle")
 #	pass
