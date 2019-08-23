91fac5ae49378c437a4c51f9409900cd8c91fcfc
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2019-08-23 18:13:04 -0300

    updates

--- Scripts/Mirror.gd
+++ Scripts/Mirror.gd
@@ -1,13 +1,20 @@
 extends Sprite
 # var a = 2
 # var b = "text"
+export var swin = true
+export var offset2 = 135
+export var size = Vector2(1024,600)
+var player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	size = Vector2(get_scale().x * 960,get_scale().y * 540)
+	player = "/root/Test/Player"
+	$Swimming.swin = swin
 	var image = get_viewport().get_texture()
 	set_texture(image) 
 
 func _process(delta):
-	if get_parent().has_node("Player"):
-		set_region_rect(Rect2(-get_parent().get_node("Player").get_global_position().x+get_global_position().x  ,get_parent().get_node("Player").get_global_position().y-get_global_position().y+135,960,500))
+	if has_node(player):
+		set_region_rect(Rect2(-get_node(player).get_global_position().x+ get_global_position().x  ,get_node(player).get_global_position().y-get_global_position().y+offset2,size.x,size.y))
 #	pass
--- Scripts/Swimming.gd
+++ Scripts/Swimming.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
+var swin = true
+
 func _on_Swimming_body_entered(body):
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and swin:
 		body.state = "swin"
 	pass # Replace with function body.
 
 func _on_Swimming_body_exited(body):
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and swin:
 		body.state = "walk"
 	pass # Replace with function body.
