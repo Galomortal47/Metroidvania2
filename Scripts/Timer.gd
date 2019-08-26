c4dd2eaf055ace4813adf40b6e13b44b344ef6ab
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2019-08-26 17:10:33 -0300

    added timer

---
+++ Scripts/Timer.gd
@@ -0,0 +1,13 @@
+extends Label
+
+var time = 360
+
+func _process(delta):
+	var hour = int(time/60)
+	var mins = int(time) - (hour *60)
+	if mins >= 10:
+		set_text(str(hour)+":"+str(mins))
+	else:
+		set_text(str(hour)+":0"+str(mins))
+	time += delta
+#	pass
