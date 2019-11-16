c94109a33ccc15e41bb781369067784a09943f64
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2019-11-15 22:13:42 -0300

    update

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,20 +1,30 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var save = preload("res://Scripts/Save.gd").new()
 var scenary = load("res://assets/DayNightcycle.tscn").instance()
-# Called when the node enters the scene tree for the first time.
+var data = {"hp":4,
+"hp_max":4,
+"level":"res://assets/Room.tscn",
+"money":0,"position_x":690,
+"position_y":460,
+ "guns" : [false,true,false,false,false,false,false,false],
+"time": 00,
+"graphics": "fast",
+"magazine" : [0,0,0,0,0,0,0,0],
+"ammo" : [0,0,0,0,0,0,0,0],
+"select" : [true,false,false,false,false,false,false,false],
+"state" : "walk",
+"cutscenes":[true,true,true,true,true,true,true,true],
+"carry" : false,
+"little_girl" : false,
+"climate" : "clear",
+"damage" : [0,1,1,1,1,1,0,0]
+}
+
 func _ready():
 	$Continue.grab_focus()
 	graphics()
 	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_Continue_button_down():
 	save.loader()
@@ -22,10 +32,10 @@ func _on_Continue_button_down():
 	pass # Replace with function body.
 
 func _on_New_Game_button_down():
+	save.data = data
 	save.save()
 	get_tree().change_scene(save.data.level)
 	pass # Replace with function body.
-
 
 func _on_Quit_Game_button_down():
 	get_tree().quit()
--- Scripts/TouchScreen.gd
+++ Scripts/TouchScreen.gd
@@ -1,6 +1,6 @@
 extends CanvasLayer
 
 func _ready():
-	if OS.get_name() == "Windows":
+	if OS.get_name() == "Windows" or OS.get_name() == "Linux":
 		$Controls.hide()
 	pass # Replace with function body.
