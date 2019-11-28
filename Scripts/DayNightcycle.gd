1fb2b7c85c6c3e3491001e64406da888f2619ad1
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2019-11-27 23:48:20 -0300

    update

--- Scripts/DayNightcycle.gd
+++ Scripts/DayNightcycle.gd
@@ -11,3 +11,10 @@ func _ready():
 	advance(save.data.time)
 #	$Rain.climate = save.data.climate
 	pass # Replace with function body.
+
+func _process(delta):
+	var color = get_node("Day Night cycle/CanvasModulate2").color
+	get_node("Day Night cycle/ParallaxBackground/CanvasModulate").color = color
+	get_node("Day Night cycle/ParallaxBackground2/CanvasModulate2").color = color
+	get_node("Day Night cycle/ParallaxBackground3/CanvasModulate3").color = color
+	get_node("Day Night cycle/Trees2/CanvasModulate4").color = color
