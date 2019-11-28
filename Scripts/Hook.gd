498463a3f76dc9f4ff59cc766ea57cfae786d40c
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2019-11-27 21:34:52 -0300

    change

--- Scripts/Hook.gd
+++ Scripts/Hook.gd
@@ -13,21 +13,25 @@ var mag_aux
 var damage
 var current = 0
 var close = 0
+var hook_path 
+
+func _ready():
+	hook_path = get_node("/root/Test/Hooks")
 
 #func _ready():
-#	if get_node("/root/Test/Hooks").get_child_count() > 0:
-#		speed = speed / get_node("/root/Test/Hooks").get_child_count()
+#	if hook_path.get_child_count() > 0:
+#		speed = speed / hook_path.get_child_count()
 
 func _physics_process(delta):
-	if have and enable and get_node("/root/Test/Hooks").get_child_count() > 0:
+	if have and enable and hook_path.get_child_count() > 0:
 		if Input.is_action_just_pressed("ui_cancel"):
 			close = get_closest()
 		if Input.is_action_pressed("ui_cancel"):
 			if get_distance(close) < rage:
-				var rot = get_angle_to(get_node("/root/Test/Hooks").get_child(close).get_global_position())
+				var rot = get_angle_to(hook_path.get_child(close).get_global_position())
 				var dir = Vector2(cos(rot), sin(rot))
 				get_parent().get_parent().motion += dir * speed
-				$Line2D.set_points([get_position(),get_node("/root/Test/Hooks").get_child(close).get_global_position()-get_global_position()])
+				$Line2D.set_points([get_position(),hook_path.get_child(close).get_global_position()-get_global_position()])
 			else:
 				$Line2D.set_points([Vector2(0,0),Vector2(0,0)])
 		else:
@@ -45,9 +49,9 @@ func reload():
 func get_closest():
 	var dist = []
 	var smallest = []
-	dist.resize(get_node("/root/Test/Hooks").get_child_count())
-	smallest.resize(get_node("/root/Test/Hooks").get_child_count())
-	for i in range(0,get_node("/root/Test/Hooks").get_child_count()):
+	dist.resize(hook_path.get_child_count())
+	smallest.resize(hook_path.get_child_count())
+	for i in range(0,hook_path.get_child_count()):
 		dist[i] = get_distance(i)
 		smallest[i] = dist[i]
 		if smallest[i] < rage2:
@@ -56,5 +60,5 @@ func get_closest():
 	return dist.find(smallest[0])
 
 func get_distance(var number):
-	var distance = get_parent().get_parent().get_global_position().distance_to(get_node("/root/Test/Hooks").get_child(number).get_global_position())
+	var distance = get_parent().get_parent().get_global_position().distance_to(hook_path.get_child(number).get_global_position())
 	return distance
