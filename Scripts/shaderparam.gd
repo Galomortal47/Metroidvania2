35e69a774eed68ab7001dd8c244679e201437553
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2019-12-06 23:43:37 -0300

    update

--- Scripts/shaderparam.gd
+++ Scripts/shaderparam.gd
@@ -2,5 +2,6 @@ extends Sprite
 
 func _process(delta):
 	var size = Vector2(32 * get_scale().x * 2,32 * get_scale().y * 2)
-	var mouse_pos = Vector2((get_global_position().x - get_global_mouse_position().x) / size.x ,(get_global_position().y - get_global_mouse_position().y) / size.y)
+	var pos = get_global_position() - get_global_mouse_position()
+	var mouse_pos = Vector2(pos .x / size.x ,pos .y / size.y)
 	get_material().set_shader_param("input",mouse_pos)
