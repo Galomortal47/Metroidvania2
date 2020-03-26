74f70d6232b3a5ccb788d56480d31dbebd5ce9cc
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2020-03-26 16:09:23 -0300

    fine tuning animation

--- externallibs/WaterRipple/example_3.gd
+++ externallibs/WaterRipple/example_3.gd
@@ -6,7 +6,7 @@ func _physics_process(delta):
 	var offset2 = (get_global_transform_with_canvas().origin / size)
 	offset2.x = 0
 	offset2.y = -((offset2.y - 1.1) * 2)
-	print(offset2)
+#	print(offset2)
 	get_node("tex_with_image").get_material().set_shader_param("own_position", offset2)
 	get_node("hv_viewport/height_velocity").get_material().set_shader_param("drop_position", (get_node('/root/singleton_player').pos_glob-get_node("tex_with_image").get_global_position()) / (Vector2(200,200)*get_parent().get_scale()))
 
