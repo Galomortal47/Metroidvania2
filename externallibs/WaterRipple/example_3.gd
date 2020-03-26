ea03b399752523d7c55c4c24c434822122dd1303
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2020-03-26 14:53:07 -0300

    water shader massive improvments

--- externallibs/WaterRipple/example_3.gd
+++ externallibs/WaterRipple/example_3.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
-func _ready():
-	get_node("hv_viewport/height_velocity").get_material()
-
 func _physics_process(delta):
-	if true:
-		get_node("hv_viewport/height_velocity").get_material().set_shader_param("drop_position", (get_node('/root/singleton_player').pos_glob-get_node("tex_with_image").get_global_position()) / (Vector2(200,200)*get_parent().get_scale()))
-	else:
-		get_node("hv_viewport/height_velocity").get_material().set_shader_param("drop_position", Vector2(-1,-1))
-#	pass
+	var size = Vector2(200,200)*get_parent().get_scale()
+	var offset = get_node('/root/singleton_player').pos_glob-get_global_position() / get_scale()
+	var offset2 = (get_global_transform_with_canvas().origin / size)
+	offset2.x = 0
+	offset2.y = -((offset2.y - 1.1) * 2)
+	print(offset2)
+	get_node("tex_with_image").get_material().set_shader_param("own_position", offset2)
+	get_node("hv_viewport/height_velocity").get_material().set_shader_param("drop_position", (get_node('/root/singleton_player').pos_glob-get_node("tex_with_image").get_global_position()) / (Vector2(200,200)*get_parent().get_scale()))
+
