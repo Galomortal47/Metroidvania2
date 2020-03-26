4cd5f8e557de3baa720d0629a4d17295fe0db97f
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2020-03-26 17:17:50 -0300

    minor improvments

--- Scripts/Swin.gd
+++ Scripts/Swin.gd
@@ -6,10 +6,13 @@ func _process(delta):
 	if is_colliding():
 		if get_collider().is_in_group("swin"):
 			get_parent().state = "swin"
+			if not water_jump:
+				if get_parent().motion.y < 400:
+					get_parent().motion.y = 0
 			water_jump = true
 	else:
 		get_parent().state = "walk"
 		if water_jump:
-			get_parent().motion.y -= 300
+			get_parent().motion.y = -500
 			water_jump = false
 #	pass
