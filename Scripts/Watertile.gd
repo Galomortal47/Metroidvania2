0a17b5e5571116071a9849bdf19d472d03dfd5fa
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2020-03-27 17:06:33 -0300

    really good looking water

--- Scripts/Watertile.gd
+++ Scripts/Watertile.gd
@@ -1,2 +1,7 @@
 extends Node2D
 
+func _ready():
+	if has_node("Viewport"):
+		var alpha = get_node("Viewport").get_texture()
+		get_node("example_2/tex_with_image").get_material().set_shader_param("alpha", alpha)
+
